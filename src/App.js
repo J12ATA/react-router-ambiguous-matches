8aef4943371b71f94578136f4f5d0b4f2defc34a
Jose Figueroa <Josefigueroalv@protonmail.com>
2018-12-26 07:49:57 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,39 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch
+} from 'react-router-dom'
+
+const About = () => <h2>About</h2>
+const Company = () => <h2>Company</h2>
+const User = ({match}) => (
+  <div>
+    <h2>User: {match.params.user}</h2>
+  </div>
+)
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      <Router>
+        <div>
+          <ul>
+            <li><Link to='/about'>About</Link></li>
+            <li><Link to='/company'>Company</Link></li>
+            <li><Link to='/kim'>Kim</Link></li>
+            <li><Link to='/chris'>Chris</Link></li>
+          </ul>
+
+          <Switch>
+            <Route path='/about' component={About} />
+            <Route path='/company' component={Company} />
+            <Route path='/:user' component={User} />
+          </Switch>
+        </div>
+      </Router>
+    )
   }
 }
 
